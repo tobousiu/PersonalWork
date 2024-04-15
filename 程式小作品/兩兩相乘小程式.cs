896f77321d849a5ca712d03ca9f08082bb86ab49
tobousiu <105833547+tobousiu@users.noreply.github.com>
2024-04-15 13:18:32 +0800

    Add files via upload

---
+++ 程式小作品/兩兩相乘小程式.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_XmultiplyX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string x = Console.ReadLine();
+                if (x == "") break;
+                int n = int.Parse(x);
+
+                for(int i=1;i<=n;i++)
+                {
+                    Console.WriteLine($"{i}*{i}={i * i}");
+                }
+            }
+        }
+    }
+}
---
+++ 程式小作品/費氏數列小程式.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                long i;
+                long ans = 1;
+                string x = Console.ReadLine();
+                if (x == "") break;
+                long n = long.Parse(x);
+
+
+               
+                for (i = 1; i <= n; i++)
+                {
+                    ans *= i;
+                }
+                
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
