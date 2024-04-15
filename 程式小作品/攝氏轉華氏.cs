98ebd9c66397687a8d55aa7e5a67e379c9acbbea
tobousiu <105833547+tobousiu@users.noreply.github.com>
2024-04-15 11:38:06 +0800

    Add files via upload

---
+++ 程式小作品/攝氏轉華氏.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_CF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double C, F;
+                String x = Console.ReadLine();
+                if (x == "") break;
+
+                C = double.Parse(x);
+                F = 9.0 / 5.0 * C + 32;
+                if(F%1==0)
+                {
+                    Console.WriteLine(F+".0");
+                }
+                else
+                {
+                    Console.WriteLine(Math.Round(F, 1));
+                }
+
+            }
+        }
+    }
+}
---
+++ 程式小作品/總時數轉秒數.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_sec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                long d, h, m, s0, s1;
+                string x = Console.ReadLine();
+                if (x == "") break;
+
+                s0 = long.Parse(x);
+                d = s0 / (60 * 60 * 24);
+                h = (s0 - (d * 60 * 60 * 24)) / (60 * 60);
+                m = (s0 - (d * 60 * 60 * 24) - (h * 60 * 60)) / 60;
+                s1 = s0 - (d * 60 * 60 * 24) - (h * 60 * 60) - (m * 60);
+                Console.WriteLine(d +" " + "days");
+                Console.WriteLine(h +" " + "hours");
+                Console.WriteLine(m +" " + "minutes");
+                Console.WriteLine(s1 +" " + "seconds");
+
+            }
+        }
+    }
+}
