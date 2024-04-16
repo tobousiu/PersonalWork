58673237a841880452a4e70319c2caaf82cab9ac
tobousiu <105833547+tobousiu@users.noreply.github.com>
2024-04-16 10:23:46 +0800

    Add files via upload

---
+++ 課程程式練習/程式考試-質數變化/Eratostosnes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSQ3
+{
+    internal class Eratostosnes
+    {
+        public int[] PrimeNo = new int[100001];
+
+        public void create()
+        {
+            PrimeNo[1] = 2; PrimeNo[2] = 3; PrimeNo[3] = 5; PrimeNo[4] = 7;
+            int N = 11;
+            int np, SqaueRoot;
+            //  Console.WriteLine("P1 = 2\nP2 = 3\nP3 = 5\nP4 = 7");
+
+            for (int n = 5; n <= 100000; n++)
+            {
+                SqaueRoot = (int)Math.Sqrt(N) + 2;
+                np = 1;
+                while (SqaueRoot >= PrimeNo[np])
+                {
+                    if (N % PrimeNo[np] == 0)
+                    {
+                        N++;
+                        np = 1;
+                    }
+                    else np++;
+                }
+                PrimeNo[n] = N;
+                // Console.WriteLine($"P{n} = {PrimeNo[n]}");
+                N++;
+            }
+        }
+
+        public bool text(long N)
+        {
+            int ptr = 1;
+            if (N < 2) return false;
+            else if (N == 2) return true;
+            else if (N % 2 == 0) return false;
+            else
+            {
+                //long SRoot = (long)Math.Sqrt(N)+1;
+                //while(SRoot >= PrimeNo[ptr])
+                while (N >= PrimeNo[ptr] * (long)PrimeNo[ptr])
+                {
+                    if (N % PrimeNo[ptr] == 0) return false;
+                    else ptr++;
+                }
+                return true;
+            }
+
+        }
+    }
+}
---
+++ 課程程式練習/程式考試-質數變化/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSQ3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Eratostosnes ET=new Eratostosnes();
+            ET.create();
+            while(true)
+            {
+                try
+                {
+                    long ch;
+                    int num = 1;
+                    Console.WriteLine(System.DateTime.Now);
+                    Console.Write("[B]Balanced prime？？ [F]Fibonacci Prime(uint)？？ [T]Twin prime(p,p+2)？");
+                    string[] x=Console.ReadLine().Split(' ');
+                    long num1=int.Parse(x[1]);
+                    long num2=int.Parse(x[2]);
+                    if(num1>num2)
+                    {
+                        ch = num1;
+                        num1 = num2;
+                        num2 = ch;
+                    }
+                    long[] P=new long[num2+2];
+
+
+                    switch (x[0])
+                    {
+                        case "B":
+
+
+                            for (int i = 1; i <= num2+1;)
+                            {
+                                num++;
+                                if (ET.text(num))
+                                {
+                                    P[i] = num;
+                                    i++;
+                                }
+                            }
+
+                            for (long j = num1; j <= num2; j++)
+                            {
+                                if((P[j-1]+P[j+1])/2==P[j])
+                                {
+                                    Console.ForegroundColor=ConsoleColor.Green;
+                                    Console.WriteLine($"P{j}= {P[j]}......Balanced prime {P[j - 1]}＋{P[j + 1]}＝{P[j]}＊２");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"P{j}= {P[j]}");
+                                }
+                            }
+
+                            break;
+
+                        case "F":
+
+                                while (num1 <= num2 && num1 >= 0)
+                                {
+                                    Console.Write($"P{num1}= {Fibonacci((uint)num1)}");
+
+                                    if (ET.text((uint)Fibonacci((uint)num1)) && num1 <= 13)
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        Console.Write(".....Prime Number");
+                                        Console.ForegroundColor = ConsoleColor.White;
+                                    }
+                                    Console.WriteLine();
+                                    num1++;
+                                }
+                            
+
+                            break;
+
+                        case "T":
+
+                            for (int i = 1; i <= num2;)
+                            {
+                                num++;
+                                if (ET.text(num))
+                                {
+                                    if (i >= num1)
+                                    {
+                                        if (ET.text(num) && ET.text(num + 2))
+                                        {
+                                            Console.ForegroundColor = ConsoleColor.Green;
+                                            Console.WriteLine($"({num},{num + 2})");
+                                            Console.ForegroundColor = ConsoleColor.White;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine($"P{i}={num}");
+                                        }
+                                    }
+                                    i++;
+                                }
+
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    Console.ForegroundColor=ConsoleColor.Red;
+                    Console.WriteLine("\n"+ex.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                finally
+                {
+                    Console.WriteLine("\n\n");
+                }
+            }
+        }
+        static decimal Fibonacci(uint X1)
+        {
+            checked
+            {
+                uint Fn = 0;
+                uint F0 = 0;
+                uint F1 = 1;
+                uint f = 2;
+                if (X1 < 0) return -1;
+                else if (X1 == 0) return 0;
+                else if (X1 == 1) return 1;
+                else
+                {
+                    while (f <= X1)
+                    {
+                        Fn = F0 + F1;
+                        F0 = F1;
+                        F1 = Fn;
+                        f++;
+                    }
+                    return Fn;
+                }
+            }
+        }
+    }
+}
