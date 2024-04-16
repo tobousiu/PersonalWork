3fd1f8a62254b5632a814fabb19d646d389d10c1
tobousiu <105833547+tobousiu@users.noreply.github.com>
2024-04-16 09:45:29 +0800

    Add files via upload

---
+++ 程式小作品/質數偵測器.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_primeYorN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string x = Console.ReadLine();
+                if (x == "") break;
+                long n = long.Parse(x);
+                if (n >= Math.Pow(2, 31) - 1) break;
+
+                int con = 0;
+                for(long i=1;i<=n;i++)
+                {
+                    if(n%i==0)
+                    {
+                        con++;
+                    }
+                }
+                if(con==2)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
