271b1bc6ed9b0cf8b4e0c80f31bc50d83eb90b97
tobousiu <105833547+tobousiu@users.noreply.github.com>
2024-04-16 09:44:10 +0800

    Add files via upload

---
+++ 程式小作品/最大質數.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_maxprime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string x = Console.ReadLine();
+                if (x == "") break;
+                int n = int.Parse(x);
+
+                int con = 0;
+                for (int i = n; i > 0; i--)
+                {
+                    for(int j=1;j<=i;j++)
+                    {
+                        if(i%j==0)
+                        {
+                            con++;
+                        }
+                    }
+                    if(con==2 && i!=n)
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                    con = 0;
+                }
+            }
+        }
+    }
+}
