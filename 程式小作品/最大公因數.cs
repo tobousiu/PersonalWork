0c43e8ebef4d5c348b36004852d74e411ec08d3f
tobousiu <105833547+tobousiu@users.noreply.github.com>
2024-04-16 09:26:36 +0800

    Add files via upload

---
+++ 程式小作品/最大公因數.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrain_Highfactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                int n1, n2,g;
+                String[] x = Console.ReadLine().Split(' ');
+                if (x.Length !=2) break;
+                n1 = int.Parse(x[0]);
+                n2 = int.Parse(x[1]);
+                if(n2>n1)
+                {
+                    (n1, n2) = (n2, n1);
+                }
+
+                while(n1%n2!=0)
+                {
+                    g = n1 % n2;
+                    n1 = n2;
+                    n2 = g;
+                }
+                Console.WriteLine($"{n2}");
+            }
+        }
+    }
+}
